892d7b83177ad2ad0b32f85c2c1bf7abb4e22b6a
aldoprogrammer <107610584+aldoprogrammer@users.noreply.github.com>
2022-07-29 08:51:28 +0700

    Add files via upload

---
+++ javascript.js
@@ -0,0 +1,67 @@
+const poke_cointainer = document.getElementById('poke_container');
+const pokemons_number = 150;
+const colors = {
+    fire: '#FDDFDF',
+    grass: '#DEFDE0',
+    electric: '#FCF7DE',
+    water: '#DEF3FD',
+    ground: '#f4e7da',
+    rock: '#d5d5d4',
+    fairy: '#fceaff',
+    posion: '#98d7a5',
+    bug: '#f8d5a3',
+    dragon: '#97b3e6',
+    psychic: '#eaeda1',
+    flying: '#f5f5f5',
+    fighting: '#E6E0D4',
+    normal: '#f5f5f5'
+};
+
+const main_types = Object.keys(colors);
+
+console.log(main_types);
+
+const fetchPokemons = async () => {
+    for (let i=1; i <= pokemons_number; i++) {
+        await getPokemon(i);
+    }
+}
+
+const getPokemon = async id => {
+    const url = `https://pokeapi.co/api/v2/pokemon/${id}`;
+    const res = await fetch(url);
+    const pokemon = await res.json();
+    createPokemonCard(pokemon);
+}
+
+fetchPokemons();
+
+function createPokemonCard(pokemon) {
+    const pokemonEl = document.createElement('div');
+    pokemonEl.classList.add('pokemon');
+
+    const poke_types = pokemon.types.map(el => el.type.name);
+    const type = main_types.find(type => poke_types.indexOf(type) > -1 );
+    const name = pokemon.name[0].toUpperCase() + pokemon.name.slice(1);
+    const color = colors[type];
+
+    pokemonEl.style.backgroundColor = color;
+
+    const pokeInnerHtml = `
+        <div class="img-container">
+        
+            <img src="https://raw.githubusercontent.com/PokeAPI/sprites/master/sprites/pokemon/${pokemon.id}.png">
+        </div>
+        <div class="info">
+            <span class="number">#${pokemon.id.toString().padStart(3, '0')}</span>
+            <h3 class="name">${name}</h3>
+            <small class="type">Type: <span>${type}</span>
+        </div>
+    `;
+
+    pokemonEl.innerHTML = pokeInnerHtml;
+
+    poke_cointainer.appendChild(pokemonEl);
+}
+
+
